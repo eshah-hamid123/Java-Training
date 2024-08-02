724a37b230683a7c69703ff7a88f9f4f76afa06a
eshah-hamid123 <eshahhamid02@gmail.com>
2024-08-02 09:56:37 +0500

    spotbug in jwtResponse.account resolved

--- BankingApp/src/main/java/com/assignment/BankingApp/security/JwtResponse.java
+++ BankingApp/src/main/java/com/assignment/BankingApp/security/JwtResponse.java
@@ -3,18 +3,22 @@ package com.assignment.BankingApp.security;
 import com.assignment.BankingApp.account.Account;
 import lombok.Getter;
 import lombok.Builder;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @Builder(toBuilder = true)
-@AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class JwtResponse {
     private String jwtToken;
     private Account account;
+
+    // Constructor with defensive copying
+    public JwtResponse(String jwtToken, Account account) {
+        this.jwtToken = jwtToken;
+        this.account = account == null ? null : new Account(account);
+    }
 
     public Account getAccount() {
         return account == null ? null : new Account(account);
